2563bcf433e5bd43f8c07132214cdbaf0923c366
AbelFiol <abelfiol2712@gmail.com>
2024-07-09 13:54:59 -0500

    Find Customer Referee

--- 584FindCustomerReferee.sql
+++ 584FindCustomerReferee.sql
@@ -1,4 +1,5 @@
-# This code work for MySQL and PostgreSQL.
+-- Select the name column from the Customer table.
 SELECT name
+-- Filter rows where referee_id is not equal to 2 or referee_id is NULL.
 FROM Customer
 WHERE referee_id <> 2 OR referee_id IS NULL;

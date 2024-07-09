edb8ae5aea614fbfe5b7124aaa6e767096015060
AbelFiol <abelfiol2712@gmail.com>
2024-07-09 13:50:54 -0500

    Recyclable and Low Fat Products

--- 1757RecyclableandLowFatProducts.sql
+++ 1757RecyclableandLowFatProducts.sql
@@ -1,4 +1,7 @@
-# This code work for MySQL and PostgreSQL.
+-- Select the product_id column from the Products table.
 SELECT product_id
+-- Filter the rows where low_fats is 'Y' (Yes).
 FROM Products
-WHERE low_fats = 'Y' AND recyclable = 'Y';
+WHERE low_fats = 'Y' 
+  -- And recyclable is 'Y' (Yes).
+  AND recyclable = 'Y';
